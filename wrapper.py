6ac13a58afc2b3cf7bb979f4efe0c52e9ef9f40b
SebastienTs <sebastien.tosi@gmail.com>
2020-01-16 17:40:29 +0100

    Updated workflow parameters

--- wrapper.py
+++ wrapper.py
@@ -20,7 +20,7 @@ def main(argv):
         # 3. Call the image analysis workflow using the run script
         nj.job.update(progress=25, statusComment="Launching workflow...")
         command = "/usr/bin/xvfb-run java -Xmx6000m -cp /fiji/jars/ij.jar ij.ImageJ --headless --console " \
-                  "-macro macro.ijm \"input={}, output={}, ij_radius={}, ij_noise={}\"".format(in_path, out_path, nj.parameters.ij_radius, nj.parameters.ij_noise)
+                  "-macro macro.ijm \"input={}, output={}, ij_rad={}, ij_thr={}, ij_noisetol={}\"".format(in_path, out_path, nj.parameters.ij_rad, nj.parameters.ij_thr, nj.parameters.ij_noisetol)
         return_code = call(command, shell=True, cwd="/fiji")  # waits for the subprocess to return
 
         if return_code != 0:
